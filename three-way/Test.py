542c16ec7c39540c5787d015f86509252ad80b93
henleyqiu <henleyqiu@tencent>
2018-08-31 17:40:53 +0800

    邱恒添加代码

---
+++ three-way/Test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 21:28:23 2017
+@author: kingqh
+"""
+
+import numpy as np
+
+def fun1(x, y):
+    return x * y
+
+
+print(fun1(2, 3))
+
